23d020c7331f535e30db0dd13bc032850391b822
AalamTasmatov <elbrusuuluruslan007@gmail.com>
2023-10-23 11:39:14 +0600

    second version

--- app.js
+++ app.js
@@ -1,68 +1,110 @@
-let nNum = document.querySelector(".nNum")
-let mNum = document.querySelector(".mNum")
-let procent =document.querySelector(".procent")
-let orel = document.querySelector(".orel")
-const work = document.querySelector(".do")
+let nNum = document.querySelector(".nNum");
+let mNum = document.querySelector(".mNum");
+let procent = document.querySelector(".procent");
+const work = document.querySelector(".do");
 
-work.onclick=()=> {
-    let arr0 = []
-    
+work.onclick = () => {
+    let arr0 = [];
+
     for (let i = 0; i < Number(mNum.value); i++) {
-        let answer0 = 0
-        
-        for(let j = 0 ; j <Number(nNum.value);j++) {
-            let bit = Math.random()
-            let bit2 = 0
-            if(bit<procent.value){
-                bit2 = 1
-            }else{
-                bit2 = 0
+        let answer0 = 0;
+
+        for (let j = 0; j < Number(nNum.value); j++) {
+            let bit = Math.random();
+            let bit2 = 0;
+            if (bit < procent.value) {
+                bit2 = 1;
+            } else {
+                bit2 = 0;
             }
 
             if (bit2 > 0) {
-                answer0++
+                answer0++;
             }
         }
-        arr0.push(answer0)
+        arr0.push(answer0);
     }
 
     const uniqueNumbers = Array.from(new Set(arr0));
     const resultArray = [];
 
     for (let i = 0; i <= Number(nNum.value); i++) {
-    if (uniqueNumbers.includes(i)) {
-        resultArray[i] = arr0.filter(item => item === i);
-    } else {
-        resultArray[i] = [];
-    }
+        if (uniqueNumbers.includes(i)) {
+            resultArray[i] = arr0.filter(item => item === i);
+        } else {
+            resultArray[i] = [];
+        }
     }
     let finalAnswer = []
-    for(let i of resultArray){
+    for (let i of resultArray) {
         finalAnswer.push(i.length)
     }
-    
-    
-    for(i of finalAnswer){
-        div1 = document.createElement("span")
-        div2 = document.createElement("div")
-        p = document.createElement("p")
-        p.innerHTML=i 
-        p.style.color="white"
-        p.style.fontSize="6px"
-        div2.style.height=`${i*2}px`
-        div2.style.width = "13px"
-        div2.style.background = "black"
-        div1.style.display="flex"
-        div1.style.flexDirection="column"
-        div1.style.alignItems="center"
-        div1.style.justifyContent="center"
+    let answ=0
 
-        div1.append(p)
-        div1.append(div2)
-        orel.append(div1)
-
+    for (let i of finalAnswer){
+        answ=answ+i
     }
+    console.log(answ);
 
-
+    
+    let ctxGaussian = document.getElementById('gaussianChart').getContext('2d');
+    let ctxBar = document.getElementById('barChart').getContext('2d')
+    let gaussianChart = new Chart(ctxGaussian, {
+        type: 'line',
+        data: {
+            labels: finalAnswer.map((_, index) => index),
+            datasets: [{
+                data: finalAnswer,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'X'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Y'
+                    }
+                }
+            }
+        }
+    })
+    let barChart = new Chart(ctxBar, {
+        type: 'bar',
+        data: {
+            labels: finalAnswer.map((_, index) => index),
+            datasets: [{
+                data: finalAnswer,
+                
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'X'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Y'
+                    }
+                }
+            }
+        }
+    })
+    let orel = document.querySelector('.orel');
+    orel.innerHTML = '';
 }
 
